072ac9897b3be7d9b14bb2962009fe770a921f1b
nathasha <nathasha36@gmail.com>
2018-10-01 13:55:27 -0700

    first first commit

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<h1> Is this working </h1> , document.getElementById('root'));
+});
